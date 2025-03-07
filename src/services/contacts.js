68a7e2a8b90fb678127535dbcb76dbe0a3c93f51
Kostenko Andrey <andrey@MacBook-Pro-Kostenko.local>
2025-03-07 10:43:34 -0300

    New branch commit

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,7 +5,31 @@ export const getAllContacts = async () => {
   return students;
 };
 
-export const getContactsById = async (studentId) => {
-  const student = await ContactsCollection.findById(studentId);
+export const getContactsById = async (contactId) => {
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const student = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+
   return student;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate( { _id: contactId }, payload, { new: true, includeResultMetadata: true, ...options });
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
 };
